67b4d9021d6c6f3b23cbe9d14ec930728215bfb1
blaster <blaster@gmail.com>
2022-12-02 19:29:51 -0600

    BAse server running

--- app/main.py
+++ app/main.py
@@ -6,3 +6,16 @@ from models import Servers as ModelServers
 from schemas import Servers
 from schemas import Servers as SchemaServers
 
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {
+        "state": "Api servers is running...."
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run()
